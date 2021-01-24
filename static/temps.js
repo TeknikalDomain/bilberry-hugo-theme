01e9bdcca03d5494b2f8c10a320eb25cb22bfd6e
Jackson W <jack@mail.tdstoragebay.com>
2021-01-24 01:23:46 -0500

    Add shortcode and associated JS for allowing users to toggle temperatures from F to C

---
+++ static/temps.js
@@ -0,0 +1,35 @@
+
+// Fired on clicking a temperature figure
+function change_temp_unit() {
+	var temp_in_f = (localStorage.getItem('temp_in_f') === 'false');
+	update_temps()
+    localStorage.setItem('temp_in_f', temp_in_f); // Toggle F <-> C and store
+}
+
+// DRY. Actually updates all the values,
+// required to run once at start to populate.
+function update_temps() {
+	var temp_in_f = (localStorage.getItem('temp_in_f') === 'true');
+
+	// Default to displaying in C if unspecificed (aka never ran before)
+	if (temp_in_f === null) {
+	    localStorage.setItem('temp_in_f', false);
+	    temp_in_f = false;
+	}
+
+	// Process each figure
+	var temps = document.getElementsByClassName('temperature-reading');
+	for (var i = 0; i < temps.length; i++) {
+	    var temp = temps[i];
+		var temp_c = parseFloat(temp.getAttribute('data-temp')); // ALWAYS in C
+		if (temp_in_f === true) {
+			// Only run conversion math if requested
+	        temp.innerHTML = ((temp_c * 1.8) + 32).toString() + "°" + "F";
+		} else {
+			temp.innerHTML = temp_c.toString() + "°" + "C";
+		}
+	}
+}
+
+// On load, run once to process everything
+update_temps()
